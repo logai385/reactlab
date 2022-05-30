a3c6933b2ee6a2157c65e0155b5842dd4991f7df
logai385 <logai385@gmail.com>
2022-05-31 06:54:37 +0700

    commit

--- src/ultil/systemSettings.js
+++ src/ultil/systemSettings.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:5000/api';
-export const URL_STATIC = 'http://localhost:5000/static';
+export const API_URL = 'http://103.184.113.83:5000/api';
+export const URL_STATIC = 'http://103.184.113.83:5000/static';
 export const STATUS_CODE={
     SUCCESS: 200,
     CREATED: 201,
